078df5a6a055f05b1fc72b505be67bb7db74b0d8
Filip <filip.kesteli@gmail.com>
2016-06-22 12:33:46 +0200

    Dodani stupci tablice u bazi podataka; onCreate i onUpgrade metoda implementirane

--- app/src/main/java/com/filipkesteli/templatesqlite/MyDBHandler.java
+++ app/src/main/java/com/filipkesteli/templatesqlite/MyDBHandler.java
@@ -1,7 +1,40 @@
 package com.filipkesteli.templatesqlite;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Filip on 22.6.2016..
  */
-public class MyDBHandler {
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "productDB.db";
+    private static final String TABLE_PRODUCTS = "products";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCT_NAME = "productname";
+    public static final String COLUMN_QUANTITY = "quantity";
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_PRODUCTS_TABLE =
+                "CREATE TABLE " + TABLE_PRODUCTS
+                        + "("
+                        + COLUMN_ID + " INTEGER PRIMARY KEY, "
+                        + COLUMN_PRODUCT_NAME + " TEXT, "
+                        + COLUMN_QUANTITY + " INTEGER"
+                        + ");";
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
+    }
 }
